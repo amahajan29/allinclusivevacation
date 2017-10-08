fa7ab0c0c7905f0f346660409987f0972895357f
Ankit <mahajan.ankit09@gmail.com>
2017-10-08 22:35:05 +0530

    Added google map and loader for package detail

--- package/controllers/package-ctrl.js
+++ package/controllers/package-ctrl.js
@@ -1,12 +1,19 @@
-var app = angular.module('package',['ui.router','checklist-model','commonServices']);
-app.controller('packageController', function($scope, $http, $templateCache, $state, $stateParams, $filter, apis,$location,$stateParams) {
+var app = angular.module('package',['ui.router','checklist-model','commonServices','ngMap']);
+app.controller('packageController', function($scope, $http, $templateCache, $state, $stateParams, $filter, apis,$location,$stateParams,NgMap) {
     $scope.packageDetails = {};
     $scope.roomDetails = {};
+    $scope.loading = true;
     $scope.displaySectoin = "availability";
     var packageObj = {LocationCode:"FAO",PackageCode:$stateParams.PackageCode};
     //var packageObj = {LocationCode:$stateParams.LocationCode,PackageCode:$stateParams.PackageCode};
     apis.packageDetail(packageObj).then(function(response){
+        $scope.loading = false;
         $scope.packageDetails = response;
+        NgMap.getMap().then(function(map) {
+            console.log(map.getCenter());
+            console.log('markers', map.markers);
+            console.log('shapes', map.shapes);
+        });
         console.log("packageDetails", response);
     }).catch(function(response) {
         console.log("Sorry, there is a problem. Please, contact support.");
@@ -18,6 +25,8 @@ app.controller('packageController', function($scope, $http, $templateCache, $sta
     }).catch(function(response) {
         console.log("Sorry, there is a problem. Please, contact support.");
     });
+
+    
 
     /*$scope.packageDetails = "111";
     $scope.testg = "222";
@@ -45,7 +54,14 @@ app.controller('packageController', function($scope, $http, $templateCache, $sta
         //$location.hash('availibility');// id of div 'availibility'
 
     };
-    //
+   /* $scope.setMap = function(){
+        var mapProp= {
+            center:new google.maps.LatLng(51.508742,-0.120850),
+            zoom:5,
+        };
+        var map=new google.maps.Map(document.getElementById("pageDetailMap"),mapProp);
+    };*/
+    //$scope.setMap();
 });
 
 /*$(".inner-nav ul li").on("click",function(){
