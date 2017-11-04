f855ebd839e06dbb5fee20d5b3e66093de2da98c
Ankit <mahajan.ankit09@gmail.com>
2017-11-05 00:50:50 +0530

    add book detail and validation

--- controllers/booking.js
+++ controllers/booking.js
@@ -4,10 +4,12 @@ app.controller('bookingController', function bookingController($scope, $http, $t
 	$scope.errors = {};
 	$scope.error = "";
 	$scope.Booking1 = {};
+	$scope.Step2 = {};
+	$scope.bookStep = 1;
 	$scope.gotoNext = function (){
 		location.href = "#/booking-step1";
 	}
-	$scope.isValidBooking1 = function(){
+	$scope.isValidStep1 = function(){
 		$scope.errors = {};
 	    $scope.error = "";
 	    var isvalid = true;
@@ -23,7 +25,34 @@ app.controller('bookingController', function bookingController($scope, $http, $t
 		}
 	    return isvalid;
 	}
+	$scope.isValidStep2 = function(){
+		$scope.errors = {};
+	    $scope.error = "";
+	    var isvalid = true;
+	    var elems = ['card','card_number','card_holder','expiration_date','cv2_number'];
+		for(var i in elems){
+			if(!$scope.Step2[elems[i]]){
+		      isvalid = false;
+		      var label = elems[i].split("_").map(function(el){ 
+		      	return el[0].toUpperCase()+el.slice(1);
+		      }).join(" ");
+		      $scope.errors[elems[i]] = label+" is required.";
+		    }
+		}
+		if(!$scope.Step2.agreement){
+			isvalid = false;
+			$scope.errors.agreement = "Please accept agreement.";
+		}
+	    return isvalid;
+	}
 	$scope.booking1Submit = function(){
-		location.href = "#/booking-details";
+		//location.href = "#/booking-details";
+		$scope.bookStep = 2;
+	}
+	$scope.gotoStep = function(step){
+		$scope.bookStep = step;
+	}
+	$scope.printBooking = function(){
+		console.log("printBooking called");
 	}
 });
